3ba922295033c73541db20f446917ca4001e4575
Annie Huang <anniehuang@Annies-MacBook-Air.local>
2023-10-01 00:58:46 -0400

    yay

--- faceLandmarks.py
+++ faceLandmarks.py
@@ -3,15 +3,17 @@ import dlib
 import numpy as np 
 import mediapipe as mp 
 
+
 cap = cv2.VideoCapture(0) # open the video camera (default camera)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 def createBox(frame, points, scale=5, masked=False,cropped=True):
+    
 
     if masked:
         mask = np.zeros_like(frame)
-        mask = cv2.fillPoly(mask,[points], (255,255,255))
+        mask = cv2.fillPoly(mask, [points], (255,255,255))
         frame = cv2.bitwise_and(frame,mask)
         # cv2.imshow("Mask", frame) # isolated cropped mask frame
 
@@ -53,13 +55,17 @@ while True:
             cv2.circle(frame, (x,y), 3, (255, 0, 0), -1)
             cv2.putText(frame, str(n), (x,y-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,0,255, 1))
 
+        print(points)
         points = np.array(points)
+        
+        
         #frameLeftEye = createBox(frame, points[36:42])
         #cv2.imshow('Lefteye', frameLeftEye)
  
         frameLips = createBox(frame, points[48:61],3,masked=True,cropped=False)
+
         frameColorLips = np.zeros_like(frameLips)
-        frameColorLips[:] = 20,0,55 # coloured frame 
+        frameColorLips[:] = 0,0,255 # coloured frame 
         frameColorLips = cv2.bitwise_and(frameLips, frameColorLips) # coloring the lips on the masked frame
         frameColorLips = cv2.GaussianBlur(frameColorLips, (7,7), 10) # blurring the edges to make it smooth
         frameColorLips = cv2.addWeighted(frame,1,frameColorLips,0.4,0)
