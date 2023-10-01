51322202ab6cd69e41da3141b3cd7eca37f1f3d0
alyssaw198 <alyssawu0104@gmail.com>
2023-09-30 22:30:01 -0400

    Code for recognizing and categorizing face shapes.

--- find_images.py
+++ find_images.py
@@ -1,35 +1,89 @@
 import os
 import cv2
 import dlib
+import cv2
+import mediapipe as mp
+import numpy as np
+from matplotlib import pyplot
+import matplotlib.colors as mcolors
+import random
+import pandas as pd
 
-
-def get_face_points(img, path_to_image):
+#uses 68 points
+def get_face_points(img):
     '''
     str --> list
-    Takes in a path to an image and then outputs the points plotted on the face of the image
+    Takes in a path to an image and then crops the image so that each face is in the same position
     '''
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
     gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = detector(gray_img)
+    image_height, image_width, _ = img.shape
 
     face_points = [[],[]]
 
     for face in faces:
-        x1,y1 = face.left(),face.top()
-        x2,y2 = face.right(),face.bottom()
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        
         landmarks = predictor(gray_img,face)
         for n in range(17):
             x = landmarks.part(n).x
             y = landmarks.part(n).y
             face_points[0].append(x)
             face_points[1].append(y)
-            cv2.circle(img,(x,y),5,(255,255,255),cv2.FILLED)
-    #cv2.imshow("Portrait", img)
+            #cv2.circle(img,(x,y),5,(255,255,255),cv2.FILLED)
+    cropped_image = img[:face_points[1][8]+10, face_points[0][0]-10:face_points[0][16]+10]
+    #cv2.imshow("Cropped", cropped_image)
     #key = cv2.waitKey(0)
+    return cropped_image
+
+
+#uses face mesh (468 points)
+def get_face_points2(img):
+    GLOBAL_WIDTH = 500
+    GLOBAL_HEIGHT = 600
+    mp_face_mesh = mp.solutions.face_mesh
+    faceMesh = mp_face_mesh.FaceMesh()
+    mp_draw = mp.solutions.drawing_utils
+
+    frame = img
+    frame = cv2.flip(frame, 1)
+    results = faceMesh.process(frame)
+
+    face_points = [[],[]]
+
+    image_height, image_width, _ = frame.shape
+    height_scale = GLOBAL_HEIGHT/image_height
+    width_scale = GLOBAL_WIDTH/image_width
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_draw.draw_landmarks(frame, face_landmarks, mp_face_mesh.FACEMESH_FACE_OVAL, mp_draw.DrawingSpec((0,255,0), 1, 1))
+
+            for id,lm in enumerate(face_landmarks.landmark):
+                ih, iw, ic = frame.shape
+                x,y = int(lm.x*iw), int(lm.y*ih)
+                face_points[0].append(x)
+                face_points[1].append(y)
+                cv2.putText(frame, str(id), (x,y), cv2.FONT_HERSHEY_TRIPLEX, 0.3, (0, 255, 0), 1)
+    
+    #print(face_points)
+    forehead_points = [face_points[0][10], face_points[1][10]]
+    cropped_image = img[forehead_points[1]+10:, :]
+    #cv2.imshow("Frame", cropped_image)
+    #key = cv2.waitKey(0)
+    new_x_points = [i * (width_scale) for i in face_points[0]]
+    new_y_points = [i * (height_scale) for i in face_points[1]]
+    face_points[0] = new_x_points
+    face_points[1] = new_y_points
     return face_points
 
-def all_face_points():    
+
+def all_face_points():     
     #get the path for current directory
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     #specify the folder name for the images
@@ -54,8 +108,44 @@ def all_face_points():
         img = cv2.imread(face_image, cv2.IMREAD_COLOR)
         category = os.path.basename(os.path.dirname(face_image))
         if category not in points:
-            points[category] = [get_face_points(img, face_image)]
+            points[category] = [get_face_points2(get_face_points(img))]
         else:
-            points[category].append(get_face_points(img, face_image))
+            points[category].append(get_face_points2(get_face_points(img)))
     return points
 
+
+def run():
+    all_faces = all_face_points()
+
+
+    def random_color():
+        '''
+        None --> Str
+        '''
+        color = random.choice(list(mcolors.CSS4_COLORS.keys()))
+        return color
+
+    face_average_points = {}
+
+    for face_type in all_faces:
+        pyplot.xlim(0, 500)
+        pyplot.ylim(150, 600)
+        all_x = []
+        all_y = []
+        for face in all_faces[face_type]:
+            all_x.append(face[0])
+            all_y.append(face[1])
+        df_x = pd.DataFrame(all_x)
+        df_y = pd.DataFrame(all_y)
+        df_x = df_x.mean()
+        df_y = df_y.mean()
+        x_avg_list = df_x.values.tolist()
+        y_avg_list = df_y.values.tolist()
+        face_average_points[face_type] = [x_avg_list, y_avg_list]
+        #print(face_type)
+        #pyplot.scatter(x_avg_list, y_avg_list, c="red", s=0.7)
+        #pyplot.show()
+    return face_average_points
+
+if __name__ == "__main__":
+    run()
